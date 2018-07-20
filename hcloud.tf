30a1e0205cf966d4a125aa3c2fd9ffcd7f872b80
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2018-07-20 23:11:33 +0200

    Added a worker node

--- hcloud.tf
+++ hcloud.tf
@@ -36,7 +36,6 @@ resource "hcloud_server" "k8s_master" {
   provisioner "remote-exec" {
     inline = [
       "sysctl net.bridge.bridge-nf-call-iptables=1",
-      "kubeadm config images pull",
       "kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${self.ipv4_address} --apiserver-cert-extra-sans=${self.ipv4_address}",
       "export KUBECONFIG=/etc/kubernetes/admin.conf",
       "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml",
@@ -52,6 +51,47 @@ output "k8s_master_ip" {
   value = "${hcloud_server.k8s_master.ipv4_address}"
 }
 
-output "k8s_master_ssh" {
-  value = "ssh root@${hcloud_server.k8s_master.ipv4_address}"
+data "external" "k8s_join" {
+  program = [
+    "./programs/k8s_master_ssh",
+    "root@${hcloud_server.k8s_master.ipv4_address}",
+    "./programs/k8s_join_data",
+  ]
+}
+
+resource "hcloud_server" "k8s_worker" {
+  count       = "1"
+  name        = "k8s-worker-${count.index + 1}"
+  server_type = "cx11"
+  image       = "debian-9"
+  location    = "nbg1"
+
+  ssh_keys = [
+    "${hcloud_ssh_key.mskrajnowski.name}",
+  ]
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update",
+      "apt-get upgrade -y",
+      "apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -",
+      "add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable'",
+      "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -",
+      "add-apt-repository 'deb http://apt.kubernetes.io/ kubernetes-xenial main'",
+      "apt-get update",
+      "apt-get install -y docker-ce kubelet kubeadm kubectl",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sysctl net.bridge.bridge-nf-call-iptables=1",
+      "kubeadm join --token ${data.external.k8s_join.result.token} --discovery-token-ca-cert-hash sha256:${data.external.k8s_join.result.hash} ${hcloud_server.k8s_master.ipv4_address}:6443",
+    ]
+  }
+}
+
+output k8s_worker_ips {
+  value = "${hcloud_server.k8s_worker.*.ipv4_address}"
 }
